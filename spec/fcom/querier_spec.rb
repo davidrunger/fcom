cb6eaa61010f6cabe9c14e2c42bd687dd0303ed3
David Runger <davidjrunger@gmail.com>
2024-07-16 23:32:29 -0500

    Fix specs

--- spec/fcom/querier_spec.rb
+++ spec/fcom/querier_spec.rb
@@ -8,40 +8,58 @@ RSpec.describe Fcom::Querier do
   describe '#query' do
     subject(:query) { querier.query }
 
-    it 'executes a #system call with the expected command' do
-      # rubocop:disable RSpec/AnyInstance
-      expect_any_instance_of(Kernel).
-        to receive(:system).
-        with(<<~COMMAND.squish)
-          git log --format="commit %s|%H|%an|%cr (%ci)" --patch --full-diff --no-textconv -- . |
-          rg "(the_search_string)|(^commit )|(^diff )" --color never |
-          fcom "the_search_string" --path . --parse-mode --repo testuser/testrepo
-        COMMAND
-      # rubocop:enable RSpec/AnyInstance
-
-      query
-    end
-
-    context 'when an author option is provided' do
-      let(:options) { stubbed_slop_options('the_search_string --author "David Runger"') }
-
-      it 'executes a #system call with the expected command' do
-        # rubocop:disable RSpec/AnyInstance
-        expect_any_instance_of(Kernel).
+    context 'when a system call indicates that the current directory exists' do
+      before do
+        expect(querier).
           to receive(:system).
-          with(<<~COMMAND.squish)
-            git log
-            --format="commit %s|%H|%an|%cr (%ci)"
-            --patch --full-diff --no-textconv
-            --author="David Runger"
-            -- .
-            |
-            rg "(the_search_string)|(^commit )|(^diff )" --color never |
-            fcom "the_search_string" --path . --parse-mode --repo testuser/testrepo
-          COMMAND
-        # rubocop:enable RSpec/AnyInstance
+          with('test -e "."').
+          and_return(true)
+      end
 
-        query
+      context 'when there have been no file renames' do
+        before do
+          expect(querier).
+            to receive(:`).
+            with("git log HEAD --format=%H --name-status --follow --diff-filter=R -- '.'").
+            and_return('')
+        end
+
+        it 'executes a backticks system call with the expected command' do
+          expect(querier).
+            to receive(:`).
+            with(<<~COMMAND.squish).
+              git log --format="commit %s|%H|%an|%cr (%ci)" --patch --full-diff --no-textconv
+                HEAD -- . |
+              rg "(the_search_string)|(^commit )|(^diff )" --color never |
+              fcom "the_search_string" --path . --parse-mode --repo testuser/testrepo
+            COMMAND
+            and_return('')
+
+          query
+        end
+
+        context 'when an author option is provided' do
+          let(:options) { stubbed_slop_options('the_search_string --author "David Runger"') }
+
+          it 'executes a backticks system call with the expected command' do
+            expect(querier).
+              to receive(:`).
+              with(<<~COMMAND.squish).
+                git log
+                --format="commit %s|%H|%an|%cr (%ci)"
+                --patch --full-diff --no-textconv
+                --author="David Runger"
+                HEAD
+                -- .
+                |
+                rg "(the_search_string)|(^commit )|(^diff )" --color never |
+                fcom "the_search_string" --path . --parse-mode --repo testuser/testrepo
+              COMMAND
+              and_return('')
+
+            query
+          end
+        end
       end
     end
   end
