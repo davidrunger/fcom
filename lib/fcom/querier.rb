davidrunger/fcom

commit 8e7ffc64a802e2bada987766c84d54bc9c26b2ac

Search back through renames [FCOM-4] (#639)
This doesn't work when the provided path (the `-p` option) is a
directory, but it fails elegantly/silently.

I suspect that this might have some bugs that need to be worked out, but
this should be good enough for now.

lib/fcom/querier.rb
@@ -5,6 +5,7 @@ require_relative 'options_helpers.rb'
 # This class executes a system command to retrieve the git history, which is passed through `rg`
 # (ripgrep), and then ultimately is fed back to `fcom` for parsing.
 class Fcom::Querier
+  prepend MemoWise
   include ::Fcom::OptionsHelpers
 
   def initialize(options)
@@ -13,6 +14,7 @@ class Fcom::Querier
 
   # rubocop:disable Metrics/CyclomaticComplexity
   # rubocop:disable Metrics/PerceivedComplexity
+  # rubocop:disable Metrics/MethodLength
   def query
     expression_to_match = search_string
     expression_to_match = Regexp.escape(expression_to_match).gsub('\\ ', ' ') unless regex_mode?
@@ -24,37 +26,88 @@ class Fcom::Querier
 
     quote = expression_to_match.include?('"') ? "'" : '"'
 
-    command = <<~COMMAND.squish
-      git log
-        --format="commit %s|%H|%an|%cr (%ci)"
-        --patch
-        --full-diff
-        --no-textconv
-        #{%(--author="#{author}") if author}
-        #{"--since=#{days}.day" unless days.nil?}
-        --
-        #{path}
-        |
+    commands =
+      filename_by_most_recent_containing_commit.map do |commit, path_at_commit|
+        <<~COMMAND.squish
+          git log
+            --format="commit %s|%H|%an|%cr (%ci)"
+            --patch
+            --full-diff
+            --no-textconv
+            #{%(--author="#{author}") if author}
+            #{"--since=#{days}.day" unless days.nil?}
+            #{commit}
+            --
+            #{path_at_commit}
+            |
 
-      rg #{quote}(#{expression_to_match})|(^commit )|(^diff )#{quote}
-        --color never
-        #{'--ignore-case' if ignore_case?}
-        #{@options[:rg_options]}
-        |
+          rg #{quote}(#{expression_to_match})|(^commit )|(^diff )#{quote}
+            --color never
+            #{'--ignore-case' if ignore_case?}
+            #{@options[:rg_options]}
+            |
 
-      #{'exe/' if development?}fcom #{quote}#{search_string}#{quote}
-        #{"--days #{days}" if days}
-        #{'--regex' if regex_mode?}
-        #{'--debug' if debug?}
-        #{'--ignore-case' if ignore_case?}
-        --path #{path}
-        --parse-mode
-        --repo #{repo}
-    COMMAND
+          #{'exe/' if development?}fcom #{quote}#{search_string}#{quote}
+            #{"--days #{days}" if days}
+            #{'--regex' if regex_mode?}
+            #{'--debug' if debug?}
+            #{'--ignore-case' if ignore_case?}
+            --path #{path_at_commit}
+            --parse-mode
+            --repo #{repo}
+        COMMAND
+      end
 
-    Fcom.logger.debug("Executing command: #{command}")
-    system(command)
+    previous_command_generated_output = false
+
+    commands.each do |command|
+      Fcom.logger.debug("Executing command: #{command}")
+      output = `#{command}`
+
+      if output.empty?
+        previous_command_generated_output = false
+      else
+        if previous_command_generated_output
+          puts("\n\n") # print blank lines for spacing
+        end
+
+        previous_command_generated_output = true
+
+        puts(output)
+      end
+    end
   end
+  # rubocop:enable Metrics/MethodLength
   # rubocop:enable Metrics/PerceivedComplexity
   # rubocop:enable Metrics/CyclomaticComplexity
+
+  private
+
+  memo_wise \
+  def filename_by_most_recent_containing_commit
+    {
+      most_recent_commit_with_file => path,
+    }.merge(renames.transform_keys { "#{_1}^" })
+  end
+
+  memo_wise \
+  def most_recent_commit_with_file
+    if system(%(test -e "#{path}"))
+      'HEAD'
+    else
+      `git log --all -1 --format="%H" -- "#{path}"`.rstrip
+    end
+  end
+
+  memo_wise \
+  def renames
+    `git log HEAD --format=%H --name-status --follow --diff-filter=R -- '#{path}'`.
+      split(/\n(?=[0-9a-f]{40})/).
+      to_h do |sha_and_name_info|
+        sha_and_name_info.
+          match(/(?<sha>[0-9a-f]{40})\n\nR\d+\s+(?<previous_name>\S+)?/).
+          named_captures.
+          values_at('sha', 'previous_name')
+      end
+  end
 end

spec/fcom/querier_spec.rb
@@ -8,40 +8,58 @@ RSpec.describe Fcom::Querier do
   describe '#query' do
     subject(:query) { querier.query }
 
-    it 'executes a #system call with the expected command' do
-      # rubocop:disable RSpec/AnyInstance
-      expect_any_instance_of(Kernel).
-        to receive(:system).
-        with(<<~COMMAND.squish)
-          git log --format="commit %s|%H|%an|%cr (%ci)" --patch --full-diff --no-textconv -- . |
-          rg "(the_search_string)|(^commit )|(^diff )" --color never |
-          fcom "the_search_string" --path . --parse-mode --repo testuser/testrepo
-        COMMAND
-      # rubocop:enable RSpec/AnyInstance
-
-      query
-    end
-
-    context 'when an author option is provided' do
-      let(:options) { stubbed_slop_options('the_search_string --author "David Runger"') }
-
-      it 'executes a #system call with the expected command' do
-        # rubocop:disable RSpec/AnyInstance
-        expect_any_instance_of(Kernel).
+    context 'when a system call indicates that the current directory exists' do
+      before do
+        expect(querier).
           to receive(:system).
-          with(<<~COMMAND.squish)
-            git log
-            --format="commit %s|%H|%an|%cr (%ci)"
-            --patch --full-diff --no-textconv
-            --author="David Runger"
-            -- .
-            |
-            rg "(the_search_string)|(^commit )|(^diff )" --color never |
-            fcom "the_search_string" --path . --parse-mode --repo testuser/testrepo
-          COMMAND
-        # rubocop:enable RSpec/AnyInstance
+          with('test -e "."').
+          and_return(true)
+      end
 
-        query
+      context 'when there have been no file renames' do
+        before do
+          expect(querier).
+            to receive(:`).
+            with("git log HEAD --format=%H --name-status --follow --diff-filter=R -- '.'").
+            and_return('')
+        end
+
+        it 'executes a backticks system call with the expected command' do
+          expect(querier).
+            to receive(:`).
+            with(<<~COMMAND.squish).
+              git log --format="commit %s|%H|%an|%cr (%ci)" --patch --full-diff --no-textconv
+                HEAD -- . |
+              rg "(the_search_string)|(^commit )|(^diff )" --color never |
+              fcom "the_search_string" --path . --parse-mode --repo testuser/testrepo
+            COMMAND
+            and_return('')
+
+          query
+        end
+
+        context 'when an author option is provided' do
+          let(:options) { stubbed_slop_options('the_search_string --author "David Runger"') }
+
+          it 'executes a backticks system call with the expected command' do
+            expect(querier).
+              to receive(:`).
+              with(<<~COMMAND.squish).
+                git log
+                --format="commit %s|%H|%an|%cr (%ci)"
+                --patch --full-diff --no-textconv
+                --author="David Runger"
+                HEAD
+                -- .
+                |
+                rg "(the_search_string)|(^commit )|(^diff )" --color never |
+                fcom "the_search_string" --path . --parse-mode --repo testuser/testrepo
+              COMMAND
+              and_return('')
+
+            query
+          end
+        end
       end
     end
   end

commit 1aaf726243a936ec23e9fc510c71490bd0b1d395

Stream results progressively [FCOM-8]
This change brings back streaming of results, which I think was broken
in #639 .

Unsurprisingly, given the complexity/overhead added to the
implementation, the fix herein does hurt performance somewhat, but not
terribly. I tested with the following queries in `david_runger` and got:

1. **with this change:** `time /home/david/code/fcom/exe/fcom
   javascript` => 15.1s, 14.6s

2. **with latest release:** `time fcom javascript` => 12.9s, 12.7s

I think that this is well worthwhile, in order to be able to start
seeing streaming results as soon as they begin to become available
(which often allows for Ctrl-C to interrupt the command before it
finishes, anyway, resulting in a net time savings, even if it is slower
if/when run all the way to completion).

Private reference link:
https://chat.deepseek.com/a/chat/s/6eb15fc5-626f-48b9-b329-a0fd6933d96a

lib/fcom/querier.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'pty'
+
 require_relative 'options_helpers.rb'
 
 # This class executes a system command to retrieve the git history, which is passed through `rg`
@@ -63,18 +65,34 @@ class Fcom::Querier
 
     commands.each do |command|
       Fcom.logger.debug("Executing command: #{command}")
-      output = `#{command}`
 
-      if output.empty?
-        previous_command_generated_output = false
-      else
-        if previous_command_generated_output
-          puts("\n\n") # print blank lines for spacing
+      # Add spacing if needed
+      if previous_command_generated_output
+        print "\n\n"
+      end
+
+      PTY.spawn(command) do |stdout, _stdin, _pid|
+        any_bytes_seen_for_command = false
+
+        # Read first byte to detect any output
+        first_byte = stdout.read(1)
+
+        any_bytes_seen_for_command = true
+
+        if first_byte
+          previous_command_generated_output = true
+
+          print(first_byte)
+
+          # Now read the rest line by line
+          stdout.each_line { puts(it) }
+        else
+          previous_command_generated_output = false
         end
-
-        previous_command_generated_output = true
-
-        puts(output)
+      rescue Errno::EIO
+        if !any_bytes_seen_for_command
+          previous_command_generated_output = false
+        end
       end
     end
   end