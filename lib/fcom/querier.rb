03ee572e8e98adab8daf7b19d1d5bb52ab4efb96
David Runger <davidjrunger@gmail.com>
2025-02-14 01:41:05 -0600

    Search rename history of . when filtering w/ directory path [FCOM-10]

--- lib/fcom/querier.rb
+++ lib/fcom/querier.rb
@@ -31,6 +31,15 @@ class Fcom::Querier
 
     commands =
       filename_by_most_recent_containing_commit.map do |commit, path_at_commit|
+        commit_without_caret = commit.first(40)
+
+        start_of_log =
+          filtered_renames.
+            drop_while { it.first != commit_without_caret }.
+            detect { it.last == path_at_commit }&.
+            first ||
+          first_commit_sha
+
         <<~COMMAND.squish
           git log
             --format="commit %s|%H|%an|%cr (%ci)"
@@ -40,7 +49,7 @@ class Fcom::Querier
             --no-textconv
             #{%(--author="#{author}") if author}
             #{days_limiter}
-            #{commit}
+            #{start_of_log}..#{commit}
             --
             #{path_at_commit}
             |
@@ -107,7 +116,7 @@ class Fcom::Querier
   def filename_by_most_recent_containing_commit
     [
       [most_recent_commit_with_file, path],
-    ] + renames.map do |commit, previous_name|
+    ] + filtered_renames.map do |commit, previous_name|
       ["#{commit}^", previous_name]
     end
   end
@@ -122,41 +131,51 @@ class Fcom::Querier
   end
 
   memo_wise \
-  def renames
-    if path == Fcom::ROOT_PATH
-      {}
-    else
-      command =
-        'git log HEAD ' \
-        "--format=%H --name-status --follow --diff-filter=R #{days_limiter} " \
-        "-- '#{path}'"
+  def filtered_renames
+    filtered_renames =
+      if path == path_for_git_log_renames_query
+        # Path is either a file or root.
+        full_renames_history_for_path_for_git_log_renames_query
+      else
+        # Path is a non-root directory.
+        already_selected = Set.new
+        path_with_trailing_slash = File.join(path, '/').to_s
 
-      Fcom.logger.debug("Querying renames with: #{command}")
-
-      renames =
-        `#{command}`.
-          split(/\n(?=[0-9a-f]{40})/).
-          flat_map do |sha_and_rename_info_lines|
-            sha, rename_info_lines =
-              sha_and_rename_info_lines.
-                match(/(?<sha>[0-9a-f]{40})\n\n(?<rename_info_lines>R\d+.*)/m).
-                named_captures.
-                values_at('sha', 'rename_info_lines')
-
-            rename_info_lines.lines.map do |rename_info_line|
-              previous_name =
-                rename_info_line.
-                  match(/\AR\d+\s+(?<previous_name>\S+)\s/).
-                  [](:previous_name)
-
-              [sha, previous_name]
+        renames_landing_in_target_directory =
+          full_renames_history_for_path_for_git_log_renames_query.
+            select do |(_commit, _previous_name, new_name)|
+              (
+                new_name.start_with?(path_with_trailing_slash) &&
+                !already_selected.include?(new_name)
+              ).tap do |is_most_recent_rename_into_target_directory|
+                if is_most_recent_rename_into_target_directory
+                  already_selected << new_name
+                end
+              end
             end
+
+        traced_back_renames(renames_landing_in_target_directory.map(&:last))
+      end
+
+    Fcom.logger.debug(<<~LOG)
+      Filtered renames:
+      #{filtered_renames.pretty_inspect}
+    LOG
+
+    filtered_renames
+  end
+
+  def traced_back_renames(new_file_names)
+    new_file_names_of_interest = Set.new(new_file_names)
+
+    full_renames_history_for_path_for_git_log_renames_query.
+      select do |(_commit, previous_name, new_name)|
+        new_file_names_of_interest.include?(new_name).tap do |is_rename_of_interest|
+          if is_rename_of_interest
+            new_file_names_of_interest << previous_name
           end
-
-      Fcom.logger.debug("Found renames: #{renames.pretty_inspect}")
-
-      renames
-    end
+        end
+      end
   end
 
   memo_wise \
@@ -164,5 +183,50 @@ class Fcom::Querier
     if days
       "--since=#{days}.day"
     end
+  end
+
+  memo_wise \
+  def full_renames_history_for_path_for_git_log_renames_query
+    command =
+      'git log HEAD ' \
+      "--format=%H --name-status --follow --diff-filter=R #{days_limiter} " \
+      "-- '#{path_for_git_log_renames_query}'"
+
+    Fcom.logger.debug("Querying renames with: #{command}")
+
+    `#{command}`.
+      split(/\n(?=[0-9a-f]{40})/).
+      flat_map do |sha_and_rename_info_lines|
+        sha, rename_info_lines =
+          sha_and_rename_info_lines.
+            match(/(?<sha>[0-9a-f]{40})\n\n(?<rename_info_lines>R\d+.*)/m).
+            named_captures.
+            values_at('sha', 'rename_info_lines')
+
+        rename_info_lines.lines.map do |rename_info_line|
+          previous_name, new_name =
+            rename_info_line.
+              match(/\AR\d+\s+(?<previous_name>\S+)\s+(?<new_name>\S+)/).
+              named_captures.
+              values_at('previous_name', 'new_name')
+
+          [sha, previous_name, new_name]
+        end
+      end.tap do |renames|
+        Fcom.logger.debug(<<~LOG)
+          Full renames history for '#{path_for_git_log_renames_query}':
+          #{renames.pretty_inspect}
+        LOG
+      end
+  end
+
+  memo_wise \
+  def path_for_git_log_renames_query
+    File.directory?(path) ? Fcom::ROOT_PATH : path
+  end
+
+  memo_wise \
+  def first_commit_sha
+    `git rev-list --max-parents=0 HEAD`.rstrip
   end
 end
