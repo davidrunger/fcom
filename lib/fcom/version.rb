98a216f9c96f9d0570be1ba8663a1e3ff0b85837
David Runger <davidjrunger@gmail.com>
2026-08-28 04:15:37 -0500

    Fix RuboCop complaint(s)

--- lib/fcom/version.rb
+++ lib/fcom/version.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-# rubocop:disable Style/StaticClass
+# rubocop:disable-next Style/StaticClass
 class Fcom
   VERSION = '0.15.1'
 end
-# rubocop:enable Style/StaticClass
